cf2bdd46c4d734fb38959e43742195bbeb936a21
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-26 21:06:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 //your JS code here. If required.
+/*
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Item Cart</title>
+</head>
+<body>
+  <table>
+    <thead>
+      <tr>
+        <th>Item</th>
+        <th>Price</th>
+        <th>Quantity</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Banana</td>
+        <td>₹1.20</td>
+        <td>3</td>
+      </tr>
+      <tr>
+        <td>Burger</td>
+        <td>₹6.90</td>
+        <td>1</td>
+      </tr>
+      <tr>
+        <td>Cookie</td>
+        <td>₹4.20</td>
+        <td>4</td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+        <td >Total</td>
+        <td>₹12.10</td>
+		<td>8</td>
+      </tr>
+    </tfoot>
+  </table>
+</body>
+</html>
+*/
